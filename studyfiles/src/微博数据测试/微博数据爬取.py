9c5a1c3cfa1058fbb5d8783602ea5c49a3843c8b
Edenman <edenman@qq.com>
2021-01-02 13:38:53 +0800

    style: 完善源码

--- studyfiles/src/微博数据测试/微博数据爬取.py
+++ studyfiles/src/微博数据测试/微博数据爬取.py
@@ -22,26 +22,50 @@ def go():
     # 二月：人民网，疫情从扩散到逐渐控制稳定
     # 三月：央视新闻，聚焦于逐渐复工复产，英雄凯旋，快递恢复等
     # 四月：人民日报，中国疫情得到全面稳定控制，各地防控有序进行，外国疫情逐渐爆发、严重
-    browser.get('https://weibo.com/cctvxinwen?is_all=1&stat_date=202005&page=')
+    browser.get('https://weibo.com/cctvxinwen?is_all=1&stat_date=202003&page=')
     sleep(30)  # 手动登录微博
     sumpage = 38
+    count = 1
+
+
 
     for page in range(1, sumpage + 1):
-        browser.get('https://weibo.com/cctvxinwen?is_all=1&stat_date=202005&page=' + str(page))
+        browser.get('https://weibo.com/cctvxinwen?is_all=1&stat_date=202003&page=' + str(page))
         sleep(7)
 
         try:
-            getContent(page, browser)
+            Infos = getContent(browser)
         except IndexError as e:
             print('第' + str(page) + '页获取失败！')
             continue
 
-        getComment(page, browser)
+        allComments = getComment(page, browser)
+
+        validNum = min(len(Infos[0]), len(Infos[1]), len(allComments))
+
+        fw = open(r'C:\Users\Ding\Desktop\Crawler-Studying\studyfiles\src\微博数据测试\微博数据\3月微博数据.txt', "w", encoding='utf-8')
+        fw.write('第' + str(count) + '条微博：\n')
+        count += 1
+
+        for i in range(0, validNum):
+            fw.write('微博内容：' + Infos[0][i] + '\n')
+            fw.write('转发数：' + Infos[1][i][0] + '\n')
+            fw.write('评论数：' + Infos[1][i][1] + '\n')
+            fw.write('点赞数：' + Infos[1][i][2] + '\n')
+            fw.write('评论内容：\n')
+
+            numOfComments = len(allComments[i])
+            for j in range(0, numOfComments):
+                fw.write(str(j + 1) + '、' + allComments[i][j] + '\n')
+
+        fw.write('----------------------------------------------------------')
+
+        fw.close()
         print('第' + str(page) + '页写入成功。')
-        sleep(10 + random.randint(1, 9))  # 设个随机的时间，不能爬太快
+        sleep(10 + random.randint(1, 5))  # 设个随机的时间，不能爬太快
 
 
-def getContent(page, browser):
+def getContent(browser):
     # 先scroll三次到最底部，每次scroll后等待4秒，加载全部45条微博内容
     browser.execute_script('window.scrollTo(0,document.body.scrollHeight)')
     sleep(12)
@@ -69,12 +93,15 @@ def getContent(page, browser):
     html = browser.page_source
 
     tree = etree.HTML(html)
+    # 用于存放45条（左右）的微博正文
+    realContents = []
+    # 用于存放每条对应的转发、评论、点赞数
+    otherInfos = []
 
     # 对正文内容的提取
     wb_text_list = tree.xpath('//div[@class="WB_text W_f14"]')
     reg = '【.+(?=展开全文)'
     pattern = re.compile(reg)
-    print(len(wb_text_list))
 
     for div in wb_text_list:
         content = div.xpath('.//text()')
@@ -85,6 +112,7 @@ def getContent(page, browser):
         realContent = re.findall(pattern, Content)[0]
         # 消去换行符等冗余符号，得到微博内容
         realContent = "".join(realContent.split())
+        realContents.append(realContent)
 
     # 对转发、评论、点赞数的获取
     row_lines = tree.xpath('//ul[@class="WB_row_line WB_row_r4 clearfix S_line2"]')
@@ -95,11 +123,14 @@ def getContent(page, browser):
         ping_lun_number = ping_lun_li[0].xpath('./a/span/span/span//text()')[1]
         zan_li = row_line.xpath('./li[4]')
         zan_number = zan_li[0].xpath('./a/span/span/span//text()')[1]
+        otherInfo = [zhuan_fa_number, ping_lun_number, zan_number]
+        otherInfos.append(otherInfo)
 
+    return [realContents, otherInfos]
 
 def getComment(page, browser):
     # 首先再请求一次网页
-    browser.get('https://weibo.com/cctvxinwen?is_all=1&stat_date=202005&page=' + str(page))
+    browser.get('https://weibo.com/cctvxinwen?is_all=1&stat_date=202003&page=' + str(page))
     sleep(8)
     # 先scroll三次到最底部，每次scroll后等待7秒，加载全部45条微博内容
     browser.execute_script('window.scrollTo(0,document.body.scrollHeight)')
@@ -121,14 +152,17 @@ def getComment(page, browser):
     html = browser.page_source
     tree = etree.HTML(html)
     ul_list = tree.xpath('//div[@class="list_ul"]')
-    reg = ''
-    pattern = re.compile(reg)
-    print(len(ul_list))
+    allComments = []
 
     for ul in ul_list:
+        comments = []
         divs = ul.xpath('./div')
         for div in divs:
             comment = div.xpath('./div[2]/div[@class="WB_text"]//text()')[2]
+            comments.append(comment)
+        allComments.append(comments)
+
+    return allComments
 
 
 
