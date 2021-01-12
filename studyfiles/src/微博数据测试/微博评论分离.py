06f5fdbccbf90f6b77c248a5de5c5bbf2c0bed38
Edenman <edenman@qq.com>
2021-01-12 23:01:02 +0800

    feat: 增加微博评论分离

---
+++ studyfiles/src/微博数据测试/微博评论分离.py
@@ -0,0 +1,27 @@
+# -*- coding: UTF-8 -*-
+import re
+
+
+path = r'C:\Users\Ding\Desktop\Crawler-Studying\微博数据\5月微博数据（央视新闻）.txt'
+txt = open(path, 'r', encoding='utf-8')
+line = txt.readline()
+newFile = open(r'C:\Users\Ding\Desktop\Crawler-Studying\微博数据\评论分离\5月评论.txt', 'w', encoding='utf-8')
+comments = []
+while line:
+    if line != '评论内容：\n':
+        line = txt.readline()
+        continue
+    else:
+        line = txt.readline()
+        while line != '----------------------------------------------------------\n':
+            line = ''.join(line.split())
+            comments.append(line[2:])
+            line = txt.readline()
+
+validComments = filter(None, comments)
+
+for comment in validComments:
+    print(comment)
+    newFile.write(comment + '\n')
+txt.close()
+newFile.close()
